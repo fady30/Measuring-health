8db0f7773cffb37f3756db5d3a19e5015e3a078a
fady30 <fady.adel.fady123@gmail.com>
2026-06-08 19:02:27 +0200

    Update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,17 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import * as fs from 'fs';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.cert'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,8 +27,13 @@ async function bootstrap(): Promise<void> {
       transform: true,
     }),
   );
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
+
   app.useGlobalFilters(new AllExceptionsFilter());
+
   app.enableCors({
     origin: true,
     credentials: true,
@@ -28,9 +41,10 @@ async function bootstrap(): Promise<void> {
 
   const configService = app.get(ConfigService);
   const port = Number(configService.get<string>('PORT') ?? '3000');
+
   await app.listen(port);
 
-  Logger.log(`API gestart op poort ${port}, Bootstrap`);
+  Logger.log(`API started on port ${port} via HTTPS`, 'Bootstrap');
 }
 
 void bootstrap();
